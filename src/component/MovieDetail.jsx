359e7118d94d06ab8a864e1b3c053d977be1cd05
rlaalsckd4 <kmc060901@gmail.com>
2025-06-18 15:01:36 +0900

    1-3 detail페이지 수정 완료

--- src/component/MovieDetail.jsx
+++ src/component/MovieDetail.jsx
@@ -1,7 +1,31 @@
-import movie from "../movieDetailData.json";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import apiToken from "../../CallToken";
 
 export default function MovieDetail() {
     const baseUrl = "https://image.tmdb.org/t/p/w500";
+    const { id } = useParams()
+    const [movie, setMovie] = useState([]);
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${apiToken}`
+        }
+    };
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}`, {
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${apiToken}`,
+            },
+        })
+            .then(res => res.json())
+            .then(data => setMovie(data))
+            .catch(err => console.error(err));
+    }, [id])
 
     return (
         <div className="max-w-5xl mx-auto px-4 py-6">
@@ -38,8 +62,8 @@ export default function MovieDetail() {
                     <div className="flex flex-wrap gap-4 text-sm">
                         <p><strong>개봉일:</strong> {movie.release_date}</p>
                         <p><strong>상영시간:</strong> {movie.runtime}분</p>
-                        <p><strong>제작비:</strong> ${movie.budget.toLocaleString()}</p>
-                        <p><strong>수익:</strong> ${movie.revenue.toLocaleString()}</p>
+                        <p><strong>제작비:</strong> ${movie.budget?.toLocaleString()}</p>
+                        <p><strong>수익:</strong> ${movie.revenue?.toLocaleString()}</p>
                     </div>
                 </div>
             </div>
