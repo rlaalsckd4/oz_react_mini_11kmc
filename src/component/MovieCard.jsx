3ff3460e861f8bf1c8e54b9e63ea7e8762284206
rlaalsckd4 <kmc060901@gmail.com>
2025-06-18 14:33:26 +0900

    MovieCard 클릭 시 ID를 포함해 이동

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom";
 
-export default function MovieCard({ title, poster_path, vote_average }) {
+export default function MovieCard({ id, title, poster_path, vote_average }) {
     const baseUrl = "https://image.tmdb.org/t/p/w500";
     const navigate = useNavigate()
 
     return (
-        <div onClick={() => navigate("/details")}>
+        <div onClick={() => navigate(`/details/${id}`)}>
             <img src={`${baseUrl}${poster_path}`} alt={title} />
             <h3>{title}</h3>
             <p>평점: {vote_average}</p>
