70729f05b2d75372021f483e9508f1e357792016
rlaalsckd4 <kmc060901@gmail.com>
2025-06-18 14:19:59 +0900

    2-2 과제 완료

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import movieData from "./movieListData.json"
 import MovieCard from "./component/MovieCard";
 import MovieSlider from "./component/MovieSlider";
+import apiToken from "../CallToken";
 
 
 export default function App() {
     const [movies, setMovies] = useState(movieData.results);
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${apiToken}`
+        }
+    };
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular', options)
+            .then(res => res.json())
+            .then(res => {
+                const childMovies = res.results.filter((movie) => movie.adult === false)
+                setMovies(childMovies)
+            })
+            .catch(err => console.error(err));
+    }, [])
 
     return (
         <div className="w-full bg-black py-6 ">
