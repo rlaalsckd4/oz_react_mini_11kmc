5e80fc517e7330b0c9053e2cc8396d97c9c23626
rlaalsckd4 <kmc060901@gmail.com>
2025-06-19 14:56:25 +0900

    route에 SearchResult 패싱

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MovieList from './MovieList.jsx'
 import MovieDetail from './component/MovieDetail.jsx'
 import Layout from './component/Layout.jsx'
+import SearchResult from './SearchResult.jsx'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
+          <Route path="/search" element={<SearchResult />} />
           <Route index element={<MovieList />} />
           <Route path='/details/:id' element={<MovieDetail />} />
         </Route>
